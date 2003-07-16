95f2f449424a14f15f9f6e3835c8fbbc2ff6e19c
Per Persson <persquare@users.sourceforge.net>
2003-07-16 20:44:29 +0000

    Splitting AQTPath into AQTPath and AQTPatch (filled polygon).

---
+++ aquaterm/AQTPatch.h
@@ -0,0 +1,17 @@
+//
+//  AQTPatch.h
+//  AquaTerm
+//
+//  Created by Per Persson on Mon Jul 14 2003.
+//  Copyright (c) 2003 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AQTPath.h"
+
+@interface AQTPatch : AQTPath
+{
+
+}
+
+@end
