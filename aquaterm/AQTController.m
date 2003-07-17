1b131ccac814a82b798220764cb4bd79cadf8695
Per Persson <persquare@users.sourceforge.net>
2003-07-17 12:49:24 +0000

    Extending tests.

--- aquaterm/AQTController.m
+++ aquaterm/AQTController.m
@@ -384,17 +384,61 @@
 
 -(IBAction)test:(id)sender
 {
-   AQTAdapter *testObject = [clientList objectAtIndex:0];
-   [testObject openPlotIndex:1 size:NSMakeSize(400,300) title:@"Testing"];
-   [testObject addLabel:@"Right" position:NSMakePoint(200,160) angle:0.0 justification:0];
-   [testObject addLabel:@"Left" position:NSMakePoint(200,150) angle:0.0 justification:1];
-   [testObject addLabel:@"Center" position:NSMakePoint(200,170) angle:0.0 justification:2];
-   [testObject addLineAtPoint:NSMakePoint(random() % 400, random() % 300)];
-   [testObject appendLineToPoint:NSMakePoint(random() % 400, random() % 300)];
-   [testObject appendLineToPoint:NSMakePoint(random() % 400, random() % 300)];
-   [testObject appendLineToPoint:NSMakePoint(random() % 400, random() % 300)];
-   [testObject appendLineToPoint:NSMakePoint(random() % 400, random() % 300)];
-   [testObject addLineAtPoint:NSMakePoint(0, 0)]; // Force end of line
-   [testObject closePlot];
+   AQTAdapter *adapter = [clientList objectAtIndex:0];
+   NSPoint polygon[5];
+   char bytes[12]={
+      255, 0, 0,
+      0, 255, 0,
+      0, 0, 255,
+      0, 0, 0
+   };
+   float xf=0.0;
+   int i;
+   int x,y;
+   if (!adapter)
+   {
+      NSLog(@"Failed to init adapter");
+   }
+   [adapter openPlotIndex:2 size:NSMakeSize(400,300) title:@"Testing"];
+   [adapter addLabel:@"Left" position:NSMakePoint(200,160) angle:0.0 justification:0];
+   [adapter addLabel:@"Center" position:NSMakePoint(200,150) angle:0.0 justification:1];
+   [adapter addLabel:@"Right" position:NSMakePoint(200,170) angle:0.0 justification:2];
+   for(i=0; i<1000; i++)
+   {
+      [adapter setColorRed:drand48() green:drand48() blue:drand48()];
+      x = random() % 360 + 20;
+      y = random() % 260 + 20;
+      [adapter addLineAtPoint:NSMakePoint(x, y)];
+      [adapter appendLineToPoint:NSMakePoint(x+random() % 10 - 5, y+random() % 10 - 5)];
+      [adapter appendLineToPoint:NSMakePoint(x+random() % 10 - 5, y+random() % 10 - 5)];
+      [adapter appendLineToPoint:NSMakePoint(x+random() % 10 - 5, y+random() % 10 - 5)];
+      [adapter appendLineToPoint:NSMakePoint(x+random() % 10 - 5, y+random() % 10 - 5)];
+   }
+   [adapter addLineAtPoint:NSMakePoint(0, 149.5)];
+   [adapter appendLineToPoint:NSMakePoint(399, 149.5)];
+   [adapter addLineAtPoint:NSMakePoint(199.5, 0)];
+   [adapter appendLineToPoint:NSMakePoint(199.5, 299)];
+   [adapter addLineAtPoint:NSMakePoint(0, 0)]; // Force end of line
+   [adapter closePlot];
+
+   [adapter openPlotIndex:3 size:NSMakeSize(200,200) title:@"Image"];
+   [adapter addImageWithBitmap:bytes size:NSMakeSize(2,2) bounds:NSMakeRect(50,50,100,100)];
+   [adapter closePlot];
+
+   [adapter openPlotIndex:4 size:NSMakeSize(200,200) title:@"Patch"];
+   polygon[0]=NSMakePoint(xf+10.0, 10.0);
+   polygon[1]=NSMakePoint(xf+20., 10.);
+   polygon[2]=NSMakePoint(xf+20., 20.);
+   polygon[3]=NSMakePoint(xf+10., 20.);
+   polygon[4]=NSMakePoint(xf+10., 10.);
+   [adapter addPolygonWithPoints:polygon pointCount:5];
+   xf=10.0;
+   polygon[0]=NSMakePoint(xf+10.0, 10.0);
+   polygon[1]=NSMakePoint(xf+20., 10.);
+   polygon[2]=NSMakePoint(xf+20., 20.);
+   polygon[3]=NSMakePoint(xf+10., 20.);
+   polygon[4]=NSMakePoint(xf+10., 10.);
+   [adapter addPolygonWithPoints:polygon pointCount:5];
+   [adapter closePlot];
 }
 @end
