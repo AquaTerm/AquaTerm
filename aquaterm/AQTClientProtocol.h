50b5fa0eff9f096762d8268c09a11f667d926d76
Per Persson <persquare@users.sourceforge.net>
2003-10-17 12:23:33 +0000

    Changes to allow event handling in client.

--- aquaterm/AQTClientProtocol.h
+++ aquaterm/AQTClientProtocol.h
@@ -1,14 +1,6 @@
 @protocol AQTClientProtocol
-
--(void)selectView:(int)aView;
-// The following methods applies to the currently selected view 
 -(void)setModel:(bycopy id)aModel; // (id)?
 -(NSDictionary *)status; // (id)?
-//-(BOOL)doCursorFromPoint:(NSPoint)startPoint withOptions:(NSDictionary *)cursorOptions;
--(void)beginMouse;
--(void)beginMouseWithOptions:(int)opt anchorPoint:(NSPoint)point;
--(BOOL)mouseIsDone;
--(char)mouseDownInfo:(inout NSPoint *)mouseLoc;
+- (void)setAcceptingEvents:(BOOL)flag;
 -(void)close;
-
 @end
--- aquaterm/AQTConnectionProtocol.h
+++ aquaterm/AQTConnectionProtocol.h
@@ -1,4 +1,6 @@
 @protocol AQTConnectionProtocol
--(id)addAQTClientWithId:(bycopy NSString *)identifier name:(bycopy NSString *)name pid:(int)procId;
--(oneway void)removeAQTClientWithId:(bycopy NSString *)identifier;
+// -(id)addAQTClientWithId:(bycopy NSString *)identifier name:(bycopy NSString *)name pid:(int)procId;
+// -(oneway void)removeAQTClientWithId:(bycopy NSString *)identifier;
+-(id)addAQTClient:(bycopy id)client name:(bycopy NSString *)name pid:(int)procId; // FIXME: bycopy??? Nooo!!?
+-(oneway void)removeAQTClient:(bycopy id)client; // FIXME: bycopy??? Nooo!!?
 @end
