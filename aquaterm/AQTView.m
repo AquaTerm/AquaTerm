51fa4e0c4afb63362903629005fc11cc0794789e
Per Persson <persquare@users.sourceforge.net>
2003-10-21 11:29:39 +0000

    Fixing leak.

--- aquaterm/AQTView.m
+++ aquaterm/AQTView.m
@@ -10,7 +10,7 @@
 #import "AQTGraphic.h"
 #import "AQTModel.h"
 
-#define AQT_MIN_FONTSIZE 9.0
+// #define AQT_MIN_FONTSIZE 9.0
 
 //#define MAX(a, b) ((a)>(b)?(a):(b))
 
@@ -19,152 +19,153 @@
 @implementation AQTView
 -(void)awakeFromNib
 {
-  NSImage *curImg = [[NSImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Cross" ofType:@"tiff"]];
-  crosshairCursor = [[NSCursor alloc] initWithImage:curImg hotSpot:NSMakePoint(7,7)];
-  [curImg release];
-  [self setIsProcessingEvents:NO];
+   NSImage *curImg = [[NSImage alloc] initWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Cross" ofType:@"tiff"]];
+   crosshairCursor = [[NSCursor alloc] initWithImage:curImg hotSpot:NSMakePoint(7,7)];
+   [curImg release];
+   [self setIsProcessingEvents:NO];
 }
 
 -(void)dealloc
 {
-  [super dealloc];
+   [crosshairCursor release];
+   [super dealloc];
 }
 
 -(BOOL)acceptsFirstResponder
 {
-  return YES;
+   return YES;
 }
 
 -(void)setModel:(AQTModel *)newModel
 {
-  model = newModel;
+   model = newModel;
 }
 
 - (AQTModel *)model
 {
-  return model;
+   return model;
 }
 
 -(BOOL)isOpaque
 {
-  return YES;
+   return YES;
 }
 
 -(BOOL)isPrinting
 {
-  return ![NSGraphicsContext currentContextDrawingToScreen]; //isPrinting;
+   return ![NSGraphicsContext currentContextDrawingToScreen]; //isPrinting;
 }
 
 /*
  -(void)setIsPrinting:(BOOL)flag
-{
-  isPrinting = flag;
-}
-*/
+ {
+    isPrinting = flag;
+ }
+ */
 
 - (BOOL)isProcessingEvents
 {
-  return _isProcessingEvents;
+   return _isProcessingEvents;
 }
 
 - (void)setIsProcessingEvents:(BOOL)flag
 {
-  _isProcessingEvents = flag;
-  [[self window] invalidateCursorRectsForView:self];
+   _isProcessingEvents = flag;
+   [[self window] invalidateCursorRectsForView:self];
 }
 
 -(void)resetCursorRects
 {
-  if ([self isProcessingEvents])
-  {
-    [self addCursorRect:[self bounds] cursor:crosshairCursor];
-  }
+   if ([self isProcessingEvents])
+   {
+      [self addCursorRect:[self bounds] cursor:crosshairCursor];
+   }
 }
 
 - (NSPoint)_convertToCanvasCoordinatesFromPoint:(NSPoint) aPoint
 {
-  NSAffineTransform *localTransform = [NSAffineTransform transform];
-  [localTransform scaleXBy:[model canvasSize].width/NSWidth([self bounds])
-                       yBy:[model canvasSize].height/NSHeight([self bounds])];
+   NSAffineTransform *localTransform = [NSAffineTransform transform];
+   [localTransform scaleXBy:[model canvasSize].width/NSWidth([self bounds])
+                        yBy:[model canvasSize].height/NSHeight([self bounds])];
 
-  return [localTransform transformPoint:aPoint]; 
+   return [localTransform transformPoint:aPoint];
 }
 -(void)mouseDown:(NSEvent *)theEvent
 {
-  if ([self isProcessingEvents])
-  {
-    NSString *eventString;
-    NSPoint pos = [self convertPoint:[theEvent locationInWindow] fromView:nil];
-    char aKey = '\0';
-    // Inform the delegate...
-    switch([theEvent type])
-    {
-      // Do some stuff here...
-      case NSLeftMouseDown:
-        // aKey = 'A';
-         NSLog(@"left key");
-        break;
-      case NSRightMouseDown:
-        // aKey = 'X';
-         NSLog(@"right key");
-        break;
-      default:
-        NSLog(@"Other key");
-        break;
-    }
-    eventString = [NSString stringWithFormat:@"1:%@:misc", NSStringFromPoint([self _convertToCanvasCoordinatesFromPoint:pos])];
-    [[[self window] delegate] setLastEvent:eventString];
-  }
+   if ([self isProcessingEvents])
+   {
+      NSString *eventString;
+      NSPoint pos = [self convertPoint:[theEvent locationInWindow] fromView:nil];
+      char aKey = '\0';
+      // Inform the delegate...
+      switch([theEvent type])
+      {
+         // Do some stuff here...
+         case NSLeftMouseDown:
+            // aKey = 'A';
+            NSLog(@"left key");
+            break;
+         case NSRightMouseDown:
+            // aKey = 'X';
+            NSLog(@"right key");
+            break;
+         default:
+            NSLog(@"Other key");
+            break;
+      }
+      eventString = [NSString stringWithFormat:@"1:%@:misc", NSStringFromPoint([self _convertToCanvasCoordinatesFromPoint:pos])];
+      [[[self window] delegate] setLastEvent:eventString];
+   }
 }
 
 -(void)keyDown:(NSEvent *)theEvent
 {
-  if ([self isProcessingEvents])
-  {
-    NSString *eventString;
-    NSPoint pos = [self convertPoint:[[self window] mouseLocationOutsideOfEventStream] fromView:nil];
-    NSRect viewBounds = [self bounds];
-    char aKey = [[theEvent characters] UTF8String][0];;
-    // NSLog([theEvent characters]);
-    if (!NSPointInRect(pos, viewBounds))
-    {
-      // Just crop it to be inside [self bounds];
-      if (pos.x < 0 )
-        pos.x = 0;
-      else if (pos.x > NSWidth(viewBounds))
-        pos.x = NSWidth(viewBounds);
-      if (pos.y < 0 )
-        pos.y = 0;
-      else if (pos.y > NSHeight(viewBounds))
-        pos.y = NSHeight(viewBounds);
-    }
-    eventString = [NSString stringWithFormat:@"2:%@:%c", NSStringFromPoint([self _convertToCanvasCoordinatesFromPoint:pos]), aKey];
-    [[[self window] delegate] setLastEvent:eventString];
-  }
+   if ([self isProcessingEvents])
+   {
+      NSString *eventString;
+      NSPoint pos = [self convertPoint:[[self window] mouseLocationOutsideOfEventStream] fromView:nil];
+      NSRect viewBounds = [self bounds];
+      char aKey = [[theEvent characters] UTF8String][0];;
+      // NSLog([theEvent characters]);
+      if (!NSPointInRect(pos, viewBounds))
+      {
+         // Just crop it to be inside [self bounds];
+         if (pos.x < 0 )
+            pos.x = 0;
+         else if (pos.x > NSWidth(viewBounds))
+            pos.x = NSWidth(viewBounds);
+         if (pos.y < 0 )
+            pos.y = 0;
+         else if (pos.y > NSHeight(viewBounds))
+            pos.y = NSHeight(viewBounds);
+      }
+      eventString = [NSString stringWithFormat:@"2:%@:%c", NSStringFromPoint([self _convertToCanvasCoordinatesFromPoint:pos]), aKey];
+      [[[self window] delegate] setLastEvent:eventString];
+   }
 }
 
--(void)drawRect:(NSRect)aRect // FIXME: Consider dirty rect!!! 
+-(void)drawRect:(NSRect)aRect // FIXME: Consider dirty rect!!!
 {
-  AQTColor canvasColor = [model color]; 
-  NSRect scaledBounds = [self bounds];	// Depends on whether we're printing or drawing to screen
-  NSAffineTransform *localTransform = [NSAffineTransform transform];
-  [localTransform scaleXBy:NSWidth(scaledBounds)/[model canvasSize].width
-                       yBy:NSHeight(scaledBounds)/[model canvasSize].height];
-  [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationNone]; // FIXME: user prefs
-  [[NSGraphicsContext currentContext] setShouldAntialias:YES]; // FIXME: user prefs
-  if (![self isPrinting])
-  {
-    //
-    // Erase by drawing background color
-    //
-    [[NSColor colorWithCalibratedRed:canvasColor.red green:canvasColor.green blue:canvasColor.blue alpha:1.0] set];
-    [[NSBezierPath bezierPathWithRect:scaledBounds] fill];
-  }
-  //
-  // Tell the model to draw itself
-  //
-  [localTransform set];
-  [model renderInRect:scaledBounds];
+   AQTColor canvasColor = [model color];
+   NSRect scaledBounds = [self bounds];	// Depends on whether we're printing or drawing to screen
+   NSAffineTransform *localTransform = [NSAffineTransform transform];
+   [localTransform scaleXBy:NSWidth(scaledBounds)/[model canvasSize].width
+                        yBy:NSHeight(scaledBounds)/[model canvasSize].height];
+   [[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationNone]; // FIXME: user prefs
+   [[NSGraphicsContext currentContext] setShouldAntialias:YES]; // FIXME: user prefs
+   if (![self isPrinting])
+   {
+      //
+      // Erase by drawing background color
+      //
+      [[NSColor colorWithCalibratedRed:canvasColor.red green:canvasColor.green blue:canvasColor.blue alpha:1.0] set];
+      [[NSBezierPath bezierPathWithRect:scaledBounds] fill];
+   }
+   //
+   // Tell the model to draw itself
+   //
+   [localTransform set];
+   [model renderInRect:scaledBounds];
 }
 
 @end
