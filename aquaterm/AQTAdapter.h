d8b14ae6bf55ca186561b1521f1357d38ea89d3a
Per Persson <persquare@users.sourceforge.net>
2004-03-16 09:15:25 +0000

    Minor changes to visible headers.

--- aquaterm/AQTAdapter.h
+++ aquaterm/AQTAdapter.h
@@ -3,7 +3,7 @@
 //  AquaTerm
 //
 //  Created by Per Persson on Sat Jul 12 2003.
-//  Copyright (c) 2003 AquaTerm. All rights reserved.
+//  Copyright (c) 2003-2004 AquaTerm.
 //
 
 #import <Foundation/NSString.h>
